2a7ce2225c4a61cdc6b917fd2d881e86c3f630ce
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-08-29 13:40:47 +0530

    style: CreatePost component added

---
+++ src/components/CreatePost.tsx
@@ -0,0 +1,101 @@
+import { Button, Input, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { styled } from "styled-components";
+
+const StyledPostContainer = styled(Box)(() => ({
+  width: 660,
+  height: 175,
+  flexShrink: 0,
+  borderRadius: 8,
+  border: "2px solid #35373B",
+  background: "#27292D",
+  display: "flex",
+  flexDirection: "column",
+  padding: "24px 20px",
+  marginBottom: "16px",
+  cursor: "pointer",
+}));
+
+const StyledPostButton = styled(Box)(() => ({
+  width: 111,
+  height: 43,
+  flexShrink: 0,
+  borderRadius: 4,
+  background: "#4A96FF",
+  color: "#FFF",
+  fontFamily: "Inter",
+  fontSize: 16,
+  fontStyle: "normal",
+  fontWeight: 500,
+  lineHeight: "normal",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const CreatePost: React.FC = () => {
+  return (
+    <StyledPostContainer>
+      <Typography
+        sx={{
+          color: "#C5C7CA",
+          fontFamily: "Inter",
+          fontSize: 18,
+          fontStyle: "normal",
+          fontWeight: 500,
+          lineHeight: "normal",
+        }}
+      >
+        Create post
+      </Typography>
+      <Box
+        sx={{
+          background: "#191920",
+          borderRadius: "8px",
+          padding: "16px",
+          display: "flex",
+          gap: "16px",
+          alignItems: "center",
+          marginTop: 2.5,
+        }}
+      >
+        <Box
+          sx={{
+            width: "48px",
+            height: "48px",
+            background: "#202326",
+            borderRadius: "50%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexShrink: 0,
+          }}
+        >
+          💬
+        </Box>
+
+        <Input
+          placeholder="How are you feeling today?"
+          disableUnderline
+          sx={{
+            placeholder: {
+              color: "#8F9093",
+            },
+            color: "white",
+            fontSize: "16px",
+            lineHeight: "normal",
+            fontFamily: "Inter",
+            width: "100%",
+          }}
+        />
+      </Box>
+      ;
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <StyledPostButton>Post</StyledPostButton>
+      </Box>
+    </StyledPostContainer>
+  );
+};
+
+export default CreatePost;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,38 @@
+import { Typography } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
+import { styled } from 'styled-components';
+import CreatePost from '../components/CreatePost';
+
+
+const StyledCaption = styled(Typography)(() => ({
+  color: "#7F8084",
+  fontFamily: "Inter",
+  fontSize: 16,
+  fontStyle: "normal",
+  fontWeight: 400,
+  lineHeight: "150%",
+ 
+}));
+
 
 const Home: React.FC = () => {
   return (
-    <div>Home</div>
+   <Container maxWidth="sm" sx={{padding:"4rem 1rem"}}>
+    <Typography sx={{
+  color: "#C5C7CA",
+  fontFamily: "Inter",
+  fontSize: "28px",
+  fontStyle: "normal",
+  fontWeight: 500,
+  lineHeight: "normal",
+ 
+}}>Hello Jane</Typography>
+    <StyledCaption>How are you doing today? Would you like to share something with the community 🤗</StyledCaption>
+    <CreatePost/>
+
+
+   </Container>
   )
 }
 
